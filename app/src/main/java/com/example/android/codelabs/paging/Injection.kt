e3110e5774fc3701e699fc556bba2cb242f47cce
Kwounsu <sb00lgs@gmail.com>
2020-08-21 11:42:12 -0700

    first commit

---
+++ app/src/main/java/com/example/android/codelabs/paging/Injection.kt
@@ -0,0 +1,32 @@
+package com.example.android.codelabs.paging
+
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.data.GithubRepository
+import com.example.android.codelabs.paging.db.RepoDatabase
+import com.example.android.codelabs.paging.ui.ViewModelFactory
+
+/**
+ * Class that handles object creation.
+ * Like this, objects can be passed as parameters in the constructors and then replaced for
+ * testing, where needed.
+ */
+object Injection {
+
+    /**
+     * Creates an instance of [GithubRepository] based on the [GithubService] and a
+     * [GithubLocalCache]
+     */
+    private fun provideGithubRepository(context: Context): GithubRepository {
+        return GithubRepository(GithubService.create(), RepoDatabase.getInstance(context))
+    }
+
+    /**
+     * Provides the [ViewModelProvider.Factory] that is then used to get a reference to
+     * [ViewModel] objects.
+     */
+    fun provideViewModelFactory(context: Context): ViewModelProvider.Factory {
+        return ViewModelFactory(provideGithubRepository(context))
+    }
+}
